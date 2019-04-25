e8a1b16eaf7bcdc3a9a80cbaa26b2885b4d45bc5
andrewdonahue <donahue00@gmail.com>
2019-04-25 13:51:55 -0400

    a bunch of  stuff

--- Blackjack/ViewController.swift
+++ Blackjack/ViewController.swift
@@ -23,11 +23,55 @@ class ViewController: UIViewController
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-        
-func roundBegin()
-    {
-    
-    }
 
+    var image1 = UIImage(named: "1-2C")
+    var image2 = UIImage(named: "2-2D")
+    var image3 = UIImage(named: "3-2H")
+    var image4 = UIImage(named: "4-2S")
+    
+    var image5 = UIImage(named: "5-3C")
+    var image6 = UIImage(named: "6-3D")
+    var image7 = UIImage(named: "7-3H")
+    var image8 = UIImage(named: "8-3S")
+    
+    
+
+
+
+
+
+    
+    func roundBegin()
+    {
+        let rand1 = Int.random(in: 1...2)
+        let rand2 = Int.random(in: 1...52)
+        let rand3 = Int.random(in: 1...52)
+        let rand4 = Int.random(in: 1...52)
+        
+        rand1 != rand2
+        rand2 != rand3
+        rand3 != rand4
+        rand4 != rand1
+        rand1 != rand3
+        rand2 != rand4
+        
+       if rand1 == 1
+       {
+        playerCard1.image = image1
+        }
+    }
+    @IBAction func RSTapped(_ sender: UIButton)
+    {
+        roundBegin()
+    }
+    @IBAction func hitTapped(_ sender: UIButton)
+    {
+        
+    }
+    @IBAction func standTapped(_ sender: UIButton)
+    {
+        
+    }
+    
 }
 
