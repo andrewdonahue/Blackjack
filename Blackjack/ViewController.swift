7adee7f62c081d1f4d85034c793a3e833dcb4f87
aengleka69 <aidanengleka@gmail.com>
2019-04-26 14:21:43 -0400

    changes

--- Blackjack/ViewController.swift
+++ Blackjack/ViewController.swift
@@ -22,6 +22,8 @@ class ViewController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        //dfgf
+        
         // Do any additional setup after loading the view, typically from a nib.
         cards = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13, image14, image15, image16, image17, image18, image19, image20, image21, image22, image23, image24, image25, image26, image27, image28, image29, image30, image31, image32, image33, image34, image35, image36, image37,] as! [UIImage]
         cards.shuffle()
