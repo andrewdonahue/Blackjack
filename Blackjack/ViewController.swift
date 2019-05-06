626cfae3d3077e35d5000595f4a701921fd90b67
aengleka69 <aidanengleka@gmail.com>
2019-05-06 14:18:37 -0400

    truth

--- Blackjack/ViewController.swift
+++ Blackjack/ViewController.swift
@@ -198,7 +198,14 @@ class ViewController: UIViewController
     //"Stand" button tapped...
     @IBAction func standTapped(_ sender: UIButton)
     {
-        if playerCard1.tag + playerCard2.tag + playerCard3.tag + playerCard4.tag + playerCard5.tag > 21
+        checkForWinner()
+    }
+    //find who is winning
+    func checkForWinner()
+    {
+        let playerHand = playerCard1.tag + playerCard2.tag + playerCard3.tag + playerCard4.tag + playerCard5.tag
+        let dealerHand = dealerCard1.tag + dealerCard2.tag
+        if playerHand > 21
         {
             let newAlert = UIAlertController(title: "You Lose!", message: "Better luck next time.", preferredStyle: .alert)
             
@@ -210,7 +217,7 @@ class ViewController: UIViewController
             
             present(newAlert, animated: true, completion: nil)
         }
-        else if  playerCard1.tag + playerCard2.tag + playerCard3.tag + playerCard4.tag + playerCard5.tag == 21 && dealerCard1.tag + dealerCard2.tag != 21
+        else if  playerHand == 21 && dealerHand != 21
         {
             let newAlert = UIAlertController(title: "You Win!", message: "BlackJack!", preferredStyle: .alert)
             
@@ -222,49 +229,48 @@ class ViewController: UIViewController
             
             present(newAlert, animated: true, completion: nil)
         }
-        else if playerCard1.tag + playerCard2.tag + playerCard3.tag + playerCard4.tag + playerCard5.tag < 21 && dealerCard1.tag + dealerCard2.tag < 21
+        else if playerHand < 21 && dealerHand < 21
         {
-           let playerScore = 21 - (playerCard1.tag + playerCard2.tag + playerCard3.tag + playerCard4.tag + playerCard5.tag)
-           let dealerScore = 21 - (dealerCard1.tag + dealerCard2.tag)
+            let playerScore = 21 - (playerCard1.tag + playerCard2.tag + playerCard3.tag + playerCard4.tag + playerCard5.tag)
+            let dealerScore = 21 - (dealerCard1.tag + dealerCard2.tag)
             
-           if playerScore > dealerScore
-           {
-            let newAlert = UIAlertController(title: "You Lose!", message: "Better luck next time.", preferredStyle: .alert)
-            
-            let ok = UIAlertAction(title: "Ok", style: .default, handler: {action in
-                newAlert.dismiss(animated: true, completion: nil)
-            })
-            
-            newAlert.addAction(ok)
-            
-            present(newAlert, animated: true, completion: nil)
+            if playerScore > dealerScore
+            {
+                let newAlert = UIAlertController(title: "You Lose!", message: "Better luck next time.", preferredStyle: .alert)
+                
+                let ok = UIAlertAction(title: "Ok", style: .default, handler: {action in
+                    newAlert.dismiss(animated: true, completion: nil)
+                })
+                
+                newAlert.addAction(ok)
+                
+                present(newAlert, animated: true, completion: nil)
             }
             else if playerScore == dealerScore
-           {
-            let newAlert = UIAlertController(title: "You Tied!", message: "Better luck next time.", preferredStyle: .alert)
-            
-            let ok = UIAlertAction(title: "Ok", style: .default, handler: {action in
-                newAlert.dismiss(animated: true, completion: nil)
-            })
-            
-            newAlert.addAction(ok)
-            
-            present(newAlert, animated: true, completion: nil)
+            {
+                let newAlert = UIAlertController(title: "You Tied!", message: "Better luck next time.", preferredStyle: .alert)
+                
+                let ok = UIAlertAction(title: "Ok", style: .default, handler: {action in
+                    newAlert.dismiss(animated: true, completion: nil)
+                })
+                
+                newAlert.addAction(ok)
+                
+                present(newAlert, animated: true, completion: nil)
             }
-           else if playerScore < dealerScore
-           {
-            let newAlert = UIAlertController(title: "You Win!", message: "BlackJack!", preferredStyle: .alert)
-            
-            let ok = UIAlertAction(title: "Ok", style: .default, handler: {action in
-                newAlert.dismiss(animated: true, completion: nil)
-            })
-            
-            newAlert.addAction(ok)
-            
-            present(newAlert, animated: true, completion: nil)
+            else if playerScore < dealerScore
+            {
+                let newAlert = UIAlertController(title: "You Win!", message: "BlackJack!", preferredStyle: .alert)
+                
+                let ok = UIAlertAction(title: "Ok", style: .default, handler: {action in
+                    newAlert.dismiss(animated: true, completion: nil)
+                })
+                
+                newAlert.addAction(ok)
+                
+                present(newAlert, animated: true, completion: nil)
             }
         }
     }
-    
 }
 
